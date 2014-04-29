9b388baeb58f475de3e76390d2db05f58ed58298
aramirez <aaron@itexpertise.net>
2014-04-29 16:38:18 +0400

    now compiling java source

--- scripts/Events.groovy
+++ scripts/Events.groovy
@@ -1,3 +1,7 @@
+import org.codehaus.gant.GantBinding
+
+includeTargets << grailsScript("_GrailsInit")
+includeTargets << grailsScript('_GrailsPackage')
 
 eventCompileStart = {
     compileScalaSource()
@@ -21,7 +25,10 @@ private void compileScalaSource() {
         def scalaSrcEncoding = 'UTF-8'
 
         println "Compiling Scala sources to $scalaClassPath"
+        ant.delete(dir: scalaClassPath)
         ant.mkdir(dir: scalaClassPath)
+
+        //1st scala compiler pass
         ant.scalac(destdir: scalaClassPath,
                 classpathref: "scala.compile.classpath",
                 encoding: scalaSrcEncoding) {
@@ -29,14 +36,36 @@ private void compileScalaSource() {
             if (new File("${basedir}/src/scala").exists()) {
                 src(path: "${basedir}/src/scala")
             }
+
             if (new File("${basedir}/src/java").exists()) {
                 src(path: "${basedir}/src/java")
             }
+        }
+
+        //compiling java classes to source dir
+        ant.javac(srcdir: "${basedir}/src/scala", destdir: scalaClassPath,
+                target: "${buildConfig.grails.project.source.level}",
+                classpathref: "scala.compile.classpath",
+                includeantruntime: false)
+
+        //2nd pass with scala compiler
+        ant.scalac(destdir: scalaClassPath,
+                classpathref: "scala.compile.classpath",
+                encoding: scalaSrcEncoding) {
+
+            if (new File("${basedir}/src/scala").exists()) {
+                src(path: "${basedir}/src/scala")
+            }
+
+            if (new File("${basedir}/src/java").exists()) {
+                src(path: "${basedir}/src/java")
+            }
+
         }
 
     } catch (Exception e) {
         Ant.fail(message: "Could not compile Scala sources: " + e.class.simpleName + ": " + e.message)
     }
 
-    println "END: Compiling Scala Source Code"
+    println "Completed: Compiling Scala Source Code"
 }
